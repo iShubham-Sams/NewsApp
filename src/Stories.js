42d527ee54aa1aee2ebbcf8e8addf41579da2a66
Shubham Bhandari <shubhambhandari12345hb@gmail.com>
2022-07-11 19:18:59 +0530

    fetch data

--- src/Stories.js
+++ src/Stories.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Stories = () => {
-  return (
-    <div>Stories</div>
+    let API ="https://hn.algolia.com/api/v1/search?query=html"
+    const fetchApiData= async ()=>{
+        try {
+            const res =await fetch(API)
+            const data =await res.json()
+            console.log(data);
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
+    useEffect(()=>{
+        fetchApiData()
+    },[])
+
+   
+  return (<>
+    <h2>My Tech News Post</h2>
+  </>
   )
 }
 
 export default Stories
+
+
+
